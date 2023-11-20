7ec2e598c82c6d619beb266c3c3e44bb091a425e
Prajakta <itsprajaktaapte@gmail.com>
2023-11-20 18:43:55 +1300

    Initial commit

---
+++ Demo/EmployeeData/Models/EmployeeViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace EmployeeData.Models
+{
+    public class EmployeeViewModel
+    {
+
+        public int Id { get; set; }
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+        [DisplayName("Date Of Birth")]
+        public DateTime DateOfBirth { get; set; }
+        [DisplayName("Email")]
+        public String Email { get; set; }
+        public double Salary { get; set; }
+        [DisplayName("Name")] 
+        public string FullName
+        {
+            get { return FirstName + "" + LastName; }
+        }
+    }
+
+}
